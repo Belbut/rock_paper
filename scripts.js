2980b2f42a31e4a976b97ffc241d29d7b5d478df
Belbut <80793911+Belbut@users.noreply.github.com>
2024-01-30 20:22:42 +0000

    Add multiple game logic and result

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 
-/* -CREATE A CONSTANT VARIABLE NAMED WEAPONS_ARRAY OF TYPE ARRAY.
-(would be best with a object but we haven't reach that part yet) */
-
 const WEAPONS_ARRAY = ["Rock", "Paper", "Scissor"];
-const GAME_RESULTS_ARRAY = ["PLAYER 1", "PLAYER 2", "DRAW"];
+const PLAYER_1_NAME = ["Player 1"]; //WILL NOT BE A CONST FOR LONG
+const PLAYER_2_NAME = ["Player 2"];
+const ROUND_RESULTS_ARRAY = [PLAYER_1_NAME, PLAYER_2_NAME, "Draw"];
 const MAKE_CHOICE_MESSAGE = "Chose your weapon wisely";
 const CANCEL_PROMPT_MESSAGE = "Player was afraid to play";
-const WONG_WEAPON_MESSAGE ="Wrong weapon please select from \"rock, paper or scissor\""
+const WONG_WEAPON_MESSAGE = "Wrong weapon please select from \"rock, paper or scissor\""
+const ROUND_RESULT_0_MESSAGE = `The battle was won by ${ROUND_RESULTS_ARRAY[0]}`;
+const ROUND_RESULT_1_MESSAGE = `The battle was won by ${ROUND_RESULTS_ARRAY[1]}`;;
+const ROUND_RESULT_2_MESSAGE = "The battle was a Draw";
+const GAME_RESULT_WINNER = `Final result player1= ${player_1_victories} and player2= ${player_2_victories} from a total of ${numberOfRounds} games.`;
+let player_1_victories = 0;
+let player_2_victories = 0;
 /*
 -CREATE FUNCTION named getComputerChoice with no needed input variable
     -create a variable named idComputerChoice
@@ -67,28 +72,66 @@ function getPlayerChoice() {
         return result from the round
     
 */
-
 function playRound(playerChoice, computerChoice) {
     let roundChoices = [playerChoice, computerChoice];
 
+    let gameResult;
     switch (roundChoices.toString()) {
         case "Rock,Scissor":
         case "Paper,Rock":
         case "Scissor,Paper":
-            return GAME_RESULTS_ARRAY[0];
- 
+            gameResult = ROUND_RESULTS_ARRAY[0];
+            break;
+
         case "Rock,Paper":
         case "Paper,Scissor":
         case "Scissor,Rock":
-            return GAME_RESULTS_ARRAY[1];
- 
+            gameResult = ROUND_RESULTS_ARRAY[1];
+            break
+
         case "Rock,Rock":
         case "Paper,Paper":
         case "Scissor,Scissor":
-            return GAME_RESULTS_ARRAY[2];
-    } 
+            gameResult = ROUND_RESULTS_ARRAY[2];
+            break;
+    
+        }
+       return presentWinner(gameResult);
+       
 }
 
-/* -CREATE FUNCTION named presentWinner
- */
-console.log(playRound(getComputerChoice(), getComputerChoice()))
+/* -CREATE FUNCTION named presentWinner() with one variable gameResult that should be one of the 3 possible game Results
+
+    -Create simple 3 case switch statement for the results and directing to the messages
+ 
+    */
+function presentWinner(gameResult) {
+    switch (gameResult) {
+        case ROUND_RESULTS_ARRAY[0]:
+            player_1_victories ++;
+            console.log(ROUND_RESULT_0_MESSAGE);
+            alert(ROUND_RESULT_0_MESSAGE);
+            break;
+
+        case ROUND_RESULTS_ARRAY[1]:
+            player_2_victories ++;
+            console.log(ROUND_RESULT_1_MESSAGE);
+            alert(ROUND_RESULT_1_MESSAGE);
+            break;
+
+        case ROUND_RESULTS_ARRAY[2]:
+            console.log(ROUND_RESULT_2_MESSAGE);
+            alert(ROUND_RESULT_2_MESSAGE);
+            break;
+    }
+    return gameResult
+}
+
+function playGame(numberOfRounds=5){
+    for (let i = 0; i < numberOfRounds; i++) {
+        playRound(getComputerChoice(),getComputerChoice())  
+    }
+    console.log(`Final result player1= ${player_1_victories} and player2= ${player_2_victories} from a total of ${numberOfRounds} games.`)
+}
+
+playGame()
